7e2dbfe08ab62d534b6e46872105f9d17fe02a7d
Kfir Cohen <kfirc@windowslive.com>
2016-08-13 21:55:54 +0300

    test

--- include/Constants.php
+++ include/Constants.php
@@ -2,7 +2,7 @@
 
 	define('HOST', 'localhost');
 	define('USER', 'root');
-	define('PASS', '12345');
+	define('PASS', '123456');
 	define('DB', 'copier');
 	
 	define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/copier/');	
